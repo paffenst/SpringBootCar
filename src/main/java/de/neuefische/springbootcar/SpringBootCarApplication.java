4134b933a896027141fed694169a94c101b581f2
paffen <pavel.stoyanov0@hm.edu>
2023-05-02 14:48:37 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/springbootcar/SpringBootCarApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.springbootcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/springbootcar/SpringBootCarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.springbootcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
